6f0a758468cdd52596901650072a7f9f0707d2b1
thanhvudev22 <154789096+thanhvudev22@users.noreply.github.com>
2024-12-19 00:19:03 +0700

    Add files via upload

---
+++ IRoom/src/BedRoom.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package iroom;
+
+/**
+ *
+ * @author tranduythanhvu05
+ */
+public class BedRoom {
+
+    private double getBaseCost() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    private String getId() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
